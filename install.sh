e2f95708575770c19dc721358e9332e395b699e7
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-11-13 19:13:10 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ function OptimizeJavaServer {
 if [ ! -f "$FILE" ]
 then
     mkdir -p plugins
-    display
+    motd
 sleep 5
 echo "
   which egg should you choose?
@@ -28,9 +28,15 @@ echo "
   3)  Node.js
 "
 
-read -r egg
-case $egg in
+read -r EGG
+case $EGG in
   1) 
     sleep 1
-    read -r version
-    echo "Downloading Forge $version..."
+    echo "enter MC version."
+    read -r MC_VERSION
+    apt install -y curl jq
+    JSON_DATA=$(curl -sSL https://files.minecraftforge.net/maven/net/minecraftforge/forge/promotions_slim.json)
+    if [[ "${MC_VERSION}" == "latest" ]] || [[ "${MC_VERSION}" == "" ]]; then
+        echo -e "Download Forge for MC version $(MC_VEESION)"
+        MC_VERSION=$(echo -e ${JSON_DATA} | jq -r '.promos | del(."latest-1.7.10") | del(."1.7.10-latest-1.7.10") | to_entries[] | .key | select(contains("latest")) | split("-")[0]' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | tail -1)rn    BUILD_TYPE=latestrn  firnrn  if [[ "${BUILD_TYPE}" != "recommended" ]] && [[ "${BUILD_TYPE}" != "latest" ]]; thenrn    BUILD_TYPE=recommendedrn  firnrn  echo -e "minecraft version: ${MC_VERSION}"rn  echo -e "build type: ${BUILD_TYPE}"rnrn
+        
