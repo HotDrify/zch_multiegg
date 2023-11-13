76d3c41d0348501883f41a842247ce6859c74333
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-11-13 20:56:48 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -33,4 +33,5 @@ case $EGG in
         echo -e "Download Forge for MC version $(MC_VEESION)"
         MC_VERSION=$(echo -e ${JSON_DATA} | jq -r '.promos | del(."latest-1.7.10") | del(."1.7.10-latest-1.7.10") | to_entries[] | .key | select(contains("latest")) | split("-")[0]' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | tail -1)
         BUILD_TYPE=latestrn    firnrn  if [[ "${BUILD_TYPE}" != "recommended" ]] && [[ "${BUILD_TYPE}" != "latest" ]]; thenrn    BUILD_TYPE=recommendedrn  firnrn  echo -e "minecraft version: ${MC_VERSION}"rn  echo -e "build type: ${BUILD_TYPE}"rnrn
+;;
 esac
