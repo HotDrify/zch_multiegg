5532f8e2a7267d140d481fb1b77266ee2a073ba6
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-11-13 20:16:14 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-function motd {
+function motd() {
     echo "ZachemHost | multiegg"
 }
 
-function JavaServer {
+function JavaServer() {
     java -Xms1024M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-server.jar nogui
 }
 
 FILE=czch.txt
 
-function OptimizeJavaServer {
+function OptimizeJavaServer() {
   echo "view-distance=6" >> server.properties
   
 }
