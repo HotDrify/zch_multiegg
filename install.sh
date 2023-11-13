fc246c1191c6c268bc70e95ef9501b4f07cc6362
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-11-13 21:17:00 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 function motd() {
     echo "ZachemHost | multiegg"
 }
@@ -25,13 +26,25 @@ read -r EGG
 case $EGG in 
 1)
     sleep 1
-    echo "enter MC version."
+    echo "enter MC version or latest"
     read -r MC_VERSION
     apt install -y curl jq
     JSON_DATA=$(curl -sSL https://files.minecraftforge.net/maven/net/minecraftforge/forge/promotions_slim.json)
     if [[ "${MC_VERSION}" == "latest" ]] || [[ "${MC_VERSION}" == "" ]]; then
-        echo -e "Download Forge for MC version $(MC_VEESION)"
-        MC_VERSION=$(echo -e ${JSON_DATA} | jq -r '.promos | del(."latest-1.7.10") | del(."1.7.10-latest-1.7.10") | to_entries[] | .key | select(contains("latest")) | split("-")[0]' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | tail -1)
-        BUILD_TYPE=latestrn    firnrn  if [[ "${BUILD_TYPE}" != "recommended" ]] && [[ "${BUILD_TYPE}" != "latest" ]]; thenrn    BUILD_TYPE=recommendedrn  firnrn  echo -e "minecraft version: ${MC_VERSION}"rn  echo -e "build type: ${BUILD_TYPE}"rnrn
-;;
+        echo "Download Forge for MC version ${MC_VERSION}"
+        MC_VERSION=$(echo -e ${JSON_DATA} | jq -r '.promos | del(."latest-1.7.10") | del(."1.7.10-latest-1.7.10") | to_entries[] | .key | select(contains("latest")) | split("-")[0]' | sort -t. -k 1,1n | tail -n 1)
+    fi
+    ;;
 esac
+
+echo "minecraft version: ${MC_VERSION}"
+
+VERSION_DATA=$(echo -e ${JSON_DATA} | jq -r '.promos["'"${MC_VERSION}"'"]')
+if [[ "${MC_VERSION)" == "latest" ]]; then
+    MC_VERSION=$(echo -e ${VERSION_DATA} | jq -r '.latest')
+else
+    MC_VERSION=$(echo -e ${VERSION_DATA} | jq -r '.recommended')
+fi
+
+echo "minecraft version: ${MC_VERSION}"
+echo "build type: ${BUILD_TYPE}"
